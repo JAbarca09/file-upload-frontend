5b49ea025a4b7f4f4fee8e683f72eb22ca833936
Jesse Abarca <JesseAbarca888@gmail.com>
2023-07-19 01:05:33 -0700

    Add React routing for Login and Sign Up pages

--- file-upload-frontend/src/App.tsx
+++ file-upload-frontend/src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import BasicForm from './components/BasicForm';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import BasicForm from "./components/BasicForm";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BasicForm isSignUp={true}/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/signup" element={<BasicForm isSignUp={true} />}></Route>
+          <Route path="/login" element={<BasicForm isSignUp={false} />}></Route>
+        </Routes>
+      </Router>
       <p>Welcome to FileFlow!</p>
     </div>
   );
