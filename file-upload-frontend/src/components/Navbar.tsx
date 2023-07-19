9296e3186c3ec520de529afb361d723563e7f03c
Jesse Abarca <JesseAbarca888@gmail.com>
2023-07-19 00:33:06 -0700

    Wrap navbar content in a div

--- file-upload-frontend/src/components/Navbar.tsx
+++ file-upload-frontend/src/components/Navbar.tsx
@@ -4,15 +4,17 @@ import styles from "./Navbar.module.css";
 const Navbar: FC = () => {
   return (
     <nav className={styles.navbar}>
-      <h1>FileFlow</h1>
-      <ul className={styles["navbar-list"]}>
-        <li>
-          <a href="#">Login</a>
-        </li>
-        <li>
-          <a href="#">Sign Up</a>
-        </li>
-      </ul>
+      <div className={styles["navbar-content-wrapper"]}>
+        <h1>FileFlow</h1>
+        <ul className={styles["navbar-list"]}>
+          <li>
+            <a href="#">Login</a>
+          </li>
+          <li>
+            <a href="#">Sign Up</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
