d474ea75f59165e84751fedb4cc13ecb10f7313b
Jesse Abarca <JesseAbarca888@gmail.com>
2023-07-22 01:19:13 -0700

    Set up FileList types FileProps and FilesListProps

--- file-upload-frontend/src/components/FileList.tsx
+++ file-upload-frontend/src/components/FileList.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import styles from "./FileList.module.css";
 
-const FileList: React.FC = () => {
-    return (
-        // name of file, date uploaded & delete button
-            <div className={styles.filelist}>
-                <ul>
-                    <li>Name</li>
-                    <li>Date</li>
-                    <li>Delete Button</li>
-                </ul>
-            </div>
-    );
+export type FileProps = {
+  name: string;
+  date: string;
+};
+
+export type FilesListProps = {
+  files: FileProps[];
+};
+
+const FileList: React.FC<FilesListProps> = ({ files }) => {
+  return (
+    <div className={styles.filelist}>
+      <ul>
+        <li>Name</li>
+        <li>Date</li>
+        <li>Delete Button</li>
+      </ul>
+    </div>
+  );
 };
 
 export default FileList;
