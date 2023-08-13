83ae123c3c6e900141f0ab52a7cc466fe7839730
Jesse Abarca <JesseAbarca888@gmail.com>
2023-08-12 18:09:37 -0700

    Create a useToast custom hook

---
+++ file-upload-frontend/src/hooks/use-toast.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+const useToast = (initialContent = "") => {
+  const [content, setContent] = useState<string>(initialContent);
+  const [showToast, setShowToast] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (content) {
+      setShowToast(true);
+
+      const timer = setTimeout(() => {
+        setShowToast(false);
+        setContent("");
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [content]);
+
+  const showToastWithContent = (newContent: string) => {
+    setContent(newContent);
+  };
+
+  return { showToast, content, showToastWithContent };
+};
+
+export default useToast;
