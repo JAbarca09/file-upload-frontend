7b6f91aa3a6fce61d6a333852c694614af2eefec
Jesse Abarca <JesseAbarca888@gmail.com>
2023-08-24 21:58:59 -0700

    Users can upload a file by dragging and dropping

--- file-upload-frontend/src/components/FileUpload.tsx
+++ file-upload-frontend/src/components/FileUpload.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import styles from "./FileUpload.module.css";
+import { uploadFile } from "../Services/DataService";
 
 const FileUpload: React.FC = () => {
   const [isDragging, setIsDragging] = useState<boolean>(false);
-  const [filename, setFilename] = useState<string>("");
+  const [filename, setFilename] = useState<string>(""); // not using this
 
   const handleDragEnter = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
@@ -23,37 +24,66 @@ const FileUpload: React.FC = () => {
   const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     setIsDragging(false);
-    handleFiles(event.dataTransfer.files);
+    const droppedFile = event.dataTransfer.files[0];
+    console.log(droppedFile);
+    handleFile(droppedFile);
   };
 
-  const handleFiles = (files: FileList) => {
-    // Handle the files here (e.g., upload, validate, etc.)
-    console.log(files);
-    setFilename(files[0].name);
+  const handleFile = async (file: File) => {
+    try {
+      // Additional validation logic can be added here before proceeding to upload
+      if (!file) {
+        console.log("No file selected.");
+        return;
+      }
+
+      const token = localStorage.getItem("jwtToken");
+
+      if (!token) {
+        console.log("Token not found. Please log in.");
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("filename", file.name);
+
+      await uploadFile(formData, token);
+    } catch (error) {
+      console.error("Error handling file:", error);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files && event.target.files[0];
+    if (selectedFile) {
+      handleFile(selectedFile);
+    }
   };
 
   return (
     <div className={styles["drop-container"]}>
-
-    <div
-      className={`${styles.dropArea} ${isDragging ? styles.dragging : ""}`}
-      onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
-      onDragOver={handleDragOver}
-      onDrop={handleDrop}
-    >
-      <label htmlFor="fileInput" className={styles.fileInputLabel}>
-        <span className={styles.chooseFileButton}>Choose File</span>
-      </label>
-      <input
-        type="file"
-        id="fileInput"
-        className={styles.fileInput}
-        onChange={(event) => handleFiles(event.target.files!)}
-      />
-      <p className={styles["drop-area-text"]}>Drag and drop files here or click to browse</p>
-      <p className={styles["drop-area-filename"]}>{filename}</p>
-    </div>
+      <div
+        className={`${styles.dropArea} ${isDragging ? styles.dragging : ""}`}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        <label htmlFor="fileInput" className={styles.fileInputLabel}>
+          <span className={styles.chooseFileButton}>Choose File</span>
+        </label>
+        <input
+          type="file"
+          id="fileInput"
+          className={styles.fileInput}
+          onChange={handleChange}
+        />
+        <p className={styles["drop-area-text"]}>
+          Drag and drop files here or click to browse
+        </p>
+        <p className={styles["drop-area-filename"]}>{filename}</p>
+      </div>
     </div>
   );
 };
