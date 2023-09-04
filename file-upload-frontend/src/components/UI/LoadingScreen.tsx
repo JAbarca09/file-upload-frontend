41e9a6bce6d76563c674ee2b6c0f51e95c91d962
Jesse Abarca <JesseAbarca888@gmail.com>
2023-09-04 16:53:40 -0700

    Apply styling to LoadingScreen.tsx

--- file-upload-frontend/src/components/UI/LoadingScreen.tsx
+++ file-upload-frontend/src/components/UI/LoadingScreen.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-// import styles from "./LoadingScreen.module.css";
+import styles from "./LoadingScreen.module.css";
 
 const LoadingScreen: React.FC = () => {
   return (
-    <div>
-      <p>Loading...</p>
+    <div className={styles["loading-screen"]}>
+      <div className={styles.spinner}></div>
+      <p className={styles["visually-hidden"]}>Loading...</p>
     </div>
   );
 };
