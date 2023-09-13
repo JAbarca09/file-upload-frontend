b7c0f3028530e24c387e8b1be7cca1a06efc5c11
Jesse Abarca <JesseAbarca888@gmail.com>
2023-09-12 19:12:33 -0700

    Display the file sizes in a human readable format

--- file-upload-frontend/src/components/FileList.tsx
+++ file-upload-frontend/src/components/FileList.tsx
@@ -12,6 +12,7 @@ export type FileProps = {
       subType: string;
     };
   };
+  size: number;
   user: {
     $oid: string;
   };
@@ -30,6 +31,46 @@ const FileList: React.FC<FilesListProps> = ({
   onFileRemove,
   onFileDownload,
 }) => {
+  
+  /**
+   * https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string
+   * Format bytes as human-readable text.
+   * 
+   * @param bytes Number of bytes.
+   * @param si True to use metric (SI) units, aka powers of 1000. False to use 
+   *           binary (IEC), aka powers of 1024.
+   * @param dp Number of decimal places to display.
+   * 
+   * @return Formatted string.
+   */
+  const humanFileSize = (
+    bytes: number,
+    si: boolean = true,
+    dp: number = 1
+  ): string => {
+    const thresh = si ? 1000 : 1024;
+
+    if (Math.abs(bytes) < thresh) {
+      return bytes + " B";
+    }
+
+    const units = si
+      ? ["KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+      : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+    let u = -1;
+    const r = 10 ** dp;
+
+    do {
+      bytes /= thresh;
+      ++u;
+    } while (
+      Math.round(Math.abs(bytes) * r) / r >= thresh &&
+      u < units.length - 1
+    );
+
+    return bytes.toFixed(dp) + " " + units[u];
+  };
+
   const FilesJsx = files.map((file) => (
     <div className={styles["file-container"]} key={file._id}>
       <div className={styles["filename-svg"]}>
@@ -41,8 +82,7 @@ const FileList: React.FC<FilesListProps> = ({
           >
             {file.filename}
           </p>
-          {/* TODO Show the fetched file size */}
-          <p>10.0 MB</p>
+          <p>{humanFileSize(file.size)}</p>
         </div>
       </div>
       <div className={styles["file-delete"]}>
